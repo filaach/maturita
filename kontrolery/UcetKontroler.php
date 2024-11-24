cd3dae53667acfe0f1f8302c73c4741069a1a1c4
Filip <chfilip2006@gmail.com>
2024-11-24 14:56:28 +0100

    ucet

--- kontrolery/UcetKontroler.php
+++ kontrolery/UcetKontroler.php
@@ -3,6 +3,7 @@ class UcetKontroler extends Kontroler
 {
     public function zpracuj(array $parametry): void
     {
+        Databaze::pripoj('localhost', 'root', '', 'maturita');
         $this->hlavicka = [
             'titulek' => 'Účet',
             'popis' => 'Stránka pro správu účtu',
@@ -10,7 +11,20 @@ class UcetKontroler extends Kontroler
             'stylesheet' => 'ucetStyles.css',
             'skripty' => []
         ];
-        $this->pohled = 'ucet';
+
+        if (empty($parametry[0]))
+            $parametry[0] = 'nastaveni';
+
+        if ($parametry[0] == 'statistiky') {
+            $this->data['comentCount'] = Databaze::dotaz("SELECT COUNT(*) AS referenced_posts_count FROM maturita.post p1 WHERE p1.post_id IN ( SELECT p2.id FROM maturita.post p2 WHERE p2.user_id = ? );", [1]);
+
+        }
+
+        $this->pohled = 'ucet/' . $parametry[0];
+
+
+
     }
+
 
 }
